c84f532d5e0c87ae304e7e3157578080cb825faf
leevick <leevick@tsinghua.edu.cn>
2020-05-17 02:09:58 +0800

    [FIX]bugs

--- axis_file_loader.srcs/sources_1/new/axis_file_loader.v
+++ axis_file_loader.srcs/sources_1/new/axis_file_loader.v
@@ -22,69 +22,137 @@
 
 module axis_file_loader #
 (
-    parameter data_file_name = "data.bin",
-    parameter result_file_name = "result.bin",
+    parameter filename = "data.bin",
     parameter data_width = 8,
-    parameter delay_ns = 100
+    parameter delay_tic = 100
 )
 (
         input axis_aclk,
         input axis_aresetn,
-        output m_axis_tvalid,
+        output reg m_axis_tvalid,
         input m_axis_tready,
-        output [8*data_width-1:0] m_axis_tdata,
-        output [data_width-1:0] m_axis_tkeep,
+        output reg[8*data_width-1:0] m_axis_tdata,
+        output reg[data_width-1:0] m_axis_tkeep,
         output m_axis_tlast,
         input cfg_prepared
     );
 
+    integer data_file = 0,rv,i=0;
+    reg eof = 0;
+    reg [7:0] buffer;
+    reg [63:0] tdata_buf[0:1];
+    reg [7:0] tkeep_buf[0:1];
 
-    reg m_axis_tvalid_buf = 0;
-    reg [8*data_width-1:0] m_axis_tdata_buf = 0;
-    reg [data_width-1:0] m_axis_tkeep_buf = 0;
-    reg m_axis_tlast_buf = 0;
+    localparam IDLE = 0;
+    localparam WAIT = 1;
+    localparam OPEN_FILE = 2;
+    localparam XFER = 3;
+    localparam XFER_LAST = 4;
 
-    assign m_axis_tvalid = m_axis_tvalid_buf;
-    assign m_axis_tdata = m_axis_tdata_buf;
-    assign m_axis_tkeep = m_axis_tkeep_buf;
-    assign m_axis_tlast = m_axis_tlast_buf;
-
-    integer data_file = 0,result_file = 0,rv,i=0,j=0;
-    reg rd_enb = 1;
-    reg [8*data_width-1:0] data_file_buffer = 0,result_file_buffer = 0;
-
-    initial begin
-        @(posedge axis_aresetn);
-        # delay_ns;
-        while(1) begin
-            @(posedge axis_aclk);
-            data_file = $fopen(data_file_name,"rb");
-            rd_enb = 1;
-            if (!data_file) begin
-                $display("data_file handle was NULL");
-            end else begin
-                $display("normal!");
-            end
-            m_axis_tvalid_buf = 1;
-            while (rd_enb) begin
-                rv = $fread(data_file_buffer,data_file);
-                m_axis_tdata_buf = 0;
-                m_axis_tkeep_buf = 8'h00;
-                for (i = 0;i<rv;i=i+1) begin
-                    m_axis_tkeep_buf[i] = 1;
-                    m_axis_tdata_buf[8*i+7-:8] = data_file_buffer[8*rv-1-8*i-:8];
+    integer state;
+    always @(posedge axis_aclk) begin
+        if (!axis_aresetn)
+            state <= IDLE;
+        else
+        case (state)
+            IDLE:
+                state <= WAIT;
+            WAIT:
+            if(delay_tic == tic)
+                state <= OPEN_FILE;
+            OPEN_FILE:
+            begin
+                data_file = $fopen(filename,"rb");
+                for (i = 0;i<data_width;i=i+1) begin
+                    rv = $fread(buffer,data_file);
+                    if(rv == 1) begin
+                        tkeep_buf[0][i] = 1;
+                        tdata_buf[0][8*i+7-:8] = buffer;
+                    end else begin
+                        tkeep_buf[0][i] = 0;
+                        tdata_buf[0][8*i+7-:8] = 0;
+                    end
                 end
-                if($feof(data_file))begin
-                    m_axis_tlast_buf = 1;
-                    rd_enb = 0;
-                end else begin
-                    m_axis_tlast_buf = 0;
-                    rd_enb = 1;
-                end
-                @(posedge axis_aclk);
+                state <= XFER;
             end
-            $fclose(data_file);
-            m_axis_tvalid_buf = 0;
+            XFER:
+            begin
+                for (i = 0;i<data_width;i=i+1) begin
+                    rv = $fread(buffer,data_file);
+                    tdata_buf[0][8*i+7-:8] = buffer;
+                    if(rv == 1)
+                        tkeep_buf[0][i] = 1;
+                    else
+                        tkeep_buf[0][i] = 0;
+                end
+                if($feof(data_file))
+                    state <= XFER_LAST;
+            end
+            XFER_LAST:
+            begin
+                state <= IDLE;
+                $fclose(data_file);
+            end
+            default:
+                state <= IDLE;
+        endcase
+    end
+
+    reg[31:0] tic;
+    wire tic_enb = state == WAIT ? 1 : 0;
+    wire tic_clr = state == XFER ? 1 : 0;
+
+    always @(posedge axis_aclk) begin
+        if (!axis_aresetn || tic_clr)
+            tic <= 0;
+        else if (tic_enb) 
+            tic <= tic + 1;
+    end
+
+    // initial begin
+    //     data_file = $fopen(filename,"rb");
+    //     @(posedge cfg_prepared);
+    //     # delay_ns;
+    //     @(posedge axis_aclk);
+    //     while (!$feof(data_file)) begin
+    //         @(posedge axis_aclk);
+    //         state = XFER;
+    //         tdata_buf[0] = 0;
+    //         tkeep_buf[0] = 0;
+    //         for (i = 0;i < data_width;i = i+1) begin
+    //             rv = $fread(buffer,data_file);
+    //             tdata_buf[0][8*i+7-:8] = buffer;
+    //             if(rv == 1)
+    //                 tkeep_buf[0][i] = 1;
+    //         end
+    //     end
+    //     state = XFER_LAST;
+    //     @(posedge axis_aclk);
+    //     state = IDLE;
+    //     $fclose(data_file);
+    // end
+
+    assign m_axis_tlast = state == XFER_LAST ? 1 : 0;
+    wire output_enb = state == XFER || state == XFER_LAST;
+    always @(posedge axis_aclk) begin
+        if (!axis_aresetn) begin
+            m_axis_tdata <= 0;
+            m_axis_tkeep <= 0;
+        end else if (output_enb) begin
+            m_axis_tdata <= tdata_buf[1];
+            m_axis_tkeep <= tkeep_buf[1];
+        end
+    end
+
+    always @(posedge axis_aclk) begin
+        if (!axis_aresetn) begin
+            tdata_buf[1] <= 0;
+            tkeep_buf[1] <= 0;
+            m_axis_tvalid <= 0;
+        end else begin
+            tdata_buf[1] <= tdata_buf[0];
+            tkeep_buf[1] <= tkeep_buf[0];
+            m_axis_tvalid <= state == XFER ? 1 : 0;
         end
     end
 
